1390b8ee5fd6011f0c2ff19b9255c9905a964b08
LorenzoF <lorenzo.fratini.4@gmail.com>
2021-04-20 11:09:39 +0200

    js update

--- js/vue-script.js
+++ js/vue-script.js
@@ -7,113 +7,93 @@ function init() {
       data: {
 
         apiKey: 'f0d1d0008218e6516fd1dca803b6c5da',
+        imgCall: 'https://image.tmdb.org/t/p/w300/',
+        maxStars: 5,
         userInput: '',
-        searchResults: [],
+        flagSrc: {
+          'en' : 'en.png',
+          'it' : 'it.png'
+        },
+        movieResults: [],
+        tvResults: [],
+        searchResults: []
       },
 
       methods: {
         
         search: function() {
 
+          
+          this.searchResults = [];
+
           axios.get('https://api.themoviedb.org/3/search/movie', {
           params: {
               'api_key': this.apiKey,
-              'query': this.userInput
+              'query': this.userInput,
+              'language' : 'it-IT'
           }
           })
           .then(data => {
             
-            this.searchResults = data.data.results;
+            this.movieResult = data.data.results;
+              
+            this.movieResult.forEach(result => {
+                  
+              result.file_type = 'movie';
 
-            // console.log(this.searchResults)
+              this.searchResults.push(result);
+            });
+
+            //console.log('movie');
+            //console.log(this.movieResult)
+
+            this.userInput = '';
           })
 
           axios.get('https://api.themoviedb.org/3/search/tv', {
-          params: {
-              'api_key': this.apiKey,
-              'query': this.userInput
-          }
-          })
-          .then(data => {
-
-            const results = data.data.results;
-
-            results.forEach(result => {
-
-              const res = result;
-
-              const newObj = {
-                "backdrop_path": res.backdrop_path,
-                "genre_ids": res.genre_ids,
-                "id": res.id,
-                "original_language": res.original_language,
-                "original_title": res.original_name,
-                "overview": res.overview,
-                "popularity": res.popularity,
-                "poster_path": res.poster_path,
-                "release_date": res.first_air_date,
-                "title": res.name,
-                "vote_average": res.vote_average,
-                "vote_count": res.vote_count
+              params: {
+                  'api_key': this.apiKey,
+                  'query': this.userInput,
+                  'language' : 'it-IT'
               }
+              })
+              .then(data => {
 
-              this.searchResults.push(newObj);
-            });
+                const results = data.data.results;
 
-            // console.log(this.searchResults)
-          })
+                this.tvResults = results;
+
+                this.tvResults.forEach(result => {
+                  
+                  result.original_title = result.original_name;
+                  result.release_date = result.first_air_date;
+                  result.title = result.name;
+                  result.file_type = 'tv';
+
+                  this.searchResults.push(result);
+                });
+
+                //console.log('tv');
+                //console.log(this.tvResults);
+                //console.log('all');
+                //console.log(this.searchResults);
+              })
         },
 
-        flagGen: function(index) {
+        starGen: function(vote) {
 
-          const result = this.searchResults[index];
-          const lenguage = result.original_language;
+          console.log('starGen');
 
-          if (lenguage == 'en') {
-            
-            return '<img src="img/en.png" alt="" class="flag"></img>'
-          } else if (lenguage == 'it') {
-            
-            return '<img src="img/it.png" alt="" class="flag"></img>'
-          } else {
-
-            return lenguage
-          }
-        },
-
-        posterGen: function(index) {
-
-          const result = this.searchResults[index];
-          const poster = result.poster_path
-
-          console.log(poster);
-
-          return 'https://image.tmdb.org/t/p/w300/' + poster;
-        },
-
-        starGen: function(index) {
-
-          const result = this.searchResults[index];
-          const vote = Math.ceil(result.vote_average / 2);
-
-          let stars = '';
-
-          if (vote == 0) {
-
-            return '<i>Nessun voto</i>'
-          }
-
-          for (i = 0; i < vote; i++) {
-
-            stars += '<i class="fas fa-star"></i>';
-          }
-          
-          return stars
+          return Math.ceil(vote / 2);
         }
       },
       
-      mounted () {
+      computed: {
 
+        stars: function() {
+
+          
+        }
       },         
     })
   }
